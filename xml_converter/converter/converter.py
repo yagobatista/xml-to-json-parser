b6279c45e639571b2b5b61a196c7e7770769726b
yago batista <yagobatistasilva@gmail.com>
2022-11-25 22:15:47 -0300

    more complex cases

--- xml_converter/converter/converter.py
+++ xml_converter/converter/converter.py
@@ -1,19 +1,43 @@
 def xml_to_dict(file: str) -> dict:
-    clean_file = file.replace('<?xml version="1.0"?>', '').replace('\n', '')
+    clean_file = file.replace(
+        '<?xml version="1.0"?>',
+        '',
+    ).replace('\n', '').strip()
     result = {}
     index = 0
 
     while index < len(clean_file):
-        opening_tag, start = get_tag(clean_file, index)
-        closing_tag, end = get_closing_tag(clean_file, opening_tag, start)
+        opening_tag, start, end, content = convertNode(
+            clean_file, index,
+        )
 
-        content = get_content(clean_file, start, end - len(closing_tag) - 2)
+        tag, _ = get_tag(content, 0)
+        has_no_sub_tag = tag == ""
+        if has_no_sub_tag:
+            result[opening_tag] = content
+            index = end + 1
+            continue
 
-        result[opening_tag] = content
+        startIndex = start + 1
+        endIndex = end - len(opening_tag) - 2
+
+        slice = clean_file[startIndex:endIndex]
+
+        sub_tags = xml_to_dict(slice)
+        result[opening_tag] = [{key: sub_tags[key]} for key in sub_tags]
 
         index = end + 1
 
     return result
+
+
+def convertNode(clean_file, index):
+    opening_tag, start = get_tag(clean_file, index)
+    closing_tag, end = get_closing_tag(clean_file, opening_tag, start)
+
+    content = get_content(clean_file, start+1, end - len(closing_tag) - 1)
+
+    return opening_tag, start, end, content
 
 
 def get_content(file: str, start: int, end: int) -> tuple[str]:
@@ -37,17 +61,22 @@ def get_closing_tag(content: str, opening_tag: str, index: int) -> tuple[str, in
 def get_tag(content: str, index: int) -> tuple[str, int]:
     tag = ""
     nextIndex = index
+    tagStarted = False
 
     while index < len(content):
         car = content[index]
-        if car == '<':
-            index += 1
-            continue
         if car == '>':
             nextIndex = index
             break
+        index += 1
+
+        if car == '<':
+            tagStarted = True
+            continue
+
+        if not tagStarted:
+            continue
 
         tag += car
-        index += 1
 
     return tag, nextIndex
--- xml_converter/converter/converter_test.py
+++ xml_converter/converter/converter_test.py
@@ -5,14 +5,70 @@ from converter.converter import xml_to_dict
 
 class XMLConverterTestCase(TestCase):
 
-    # def test_empty_file(self):
-    #     result = xml_to_dict("")
-    #     self.assertEqual({}, result)
+    def test_empty_file(self):
+        result = xml_to_dict("")
+        self.assertEqual({}, result)
 
-    # def test_remove_xml_tag(self):
-    #     result = xml_to_dict('<?xml version="1.0"?>')
-    #     self.assertEqual({}, result)
+    def test_remove_xml_tag(self):
+        result = xml_to_dict('<?xml version="1.0"?>')
+        self.assertEqual({}, result)
 
     def test_basic_case(self):
         result = xml_to_dict('<?xml version="1.0"?>\n<Root></Root>\n')
         self.assertEqual({"Root": ""}, result)
+
+    def test_sub_nodes(self):
+        result = xml_to_dict(
+            '<Address><StreetLine1>400 Market St.</StreetLine1><City>San Francisco</City><State>CA</State><PostCode>94108</PostCode></Address>'
+        )
+        self.assertEqual(
+            {
+                "Address": [
+                    {"StreetLine1": "400 Market St."},
+                    {"City": "San Francisco"},
+                    {"State": "CA"},
+                    {"PostCode": "94108"},
+                ]
+            },
+            result,
+        )
+
+    def test_complex_sub_nodes(self):
+        result = xml_to_dict(
+            '<TestRoot><Address><StreetLine1>400 Market St.</StreetLine1></Address><State>SP</State></TestRoot>'
+        )
+        self.assertEqual(
+            {
+                'TestRoot': [
+                    {
+                        "Address": [
+                            {"StreetLine1": "400 Market St."},
+                        ]
+                    },
+                    {
+                        "State": "SP",
+                    }
+                ]
+            },
+            result,
+        )
+
+    def test_complex_Wwith_same_sub_nodes(self):
+        result = xml_to_dict(
+            '<TestRoot><Address><StreetLine1>400 Market St.</StreetLine1></Address><Address>SP</Address></TestRoot>'
+        )
+        self.assertEqual(
+            {
+                'TestRoot': [
+                    {
+                        "Address": [
+                            {"StreetLine1": "400 Market St."},
+                        ]
+                    },
+                    {
+                        "Address": "SP",
+                    }
+                ]
+            },
+            result,
+        )
